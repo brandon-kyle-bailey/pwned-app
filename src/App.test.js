ed852d14f4d88ab85591a3c69941af2b3afca864
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-04-14 19:03:57 -0300

    updating test to search for h1 element of main page

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Can I use this domain?', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Can I use this domain?/i);
   expect(linkElement).toBeInTheDocument();
 });
